2d3dafaf86121f2f38db35184a7087fd6430f137
PatrickBoynton <jpboynton3@gmail.com>
2021-01-25 16:58:01 -0500

    Added a toggle that isn't supposed to work right.

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const button = document.querySelector(".switch");
+const img1 = document.querySelector("#clip");
+const img2 = document.querySelector("#clip2");
 
+
+// Not supposed to actually flip the images.
 button.addEventListener("click", () => {
-  alert("Test!");
-})
+  img1.src = img2.src;
+});
